a87e4a661894f2e47010cafeb121c87f597a0036
jcopado <jcopado@surlabs.es>
2023-09-14 23:30:56 +0200

    2023.09.14 - primaryExistsByFields method for AbstractILIASDatabaseDetector is required in ILIAS 7.25

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = 'xlvo';
 $version = '2023.09.14';
-$ilias_min_version = '7.25';
+$ilias_min_version = '6.99';
 $ilias_max_version = '7.999';
 $responsible = 'fluxlabs ag';
 $responsible_mail = 'support@fluxlabs.ch';
